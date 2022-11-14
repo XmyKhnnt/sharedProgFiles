992d2a989be96707d696a2b3e29a4a57f921f580
Khent Mark Dahay <khentmark27dahay@gmail.com>
2022-11-15 00:37:04 +0800

    pyqt5 course source code

--- main.py
+++ main.py
@@ -1,28 +1,81 @@
 
 
+import sys
 
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout
 
+)
 
-
-
-os.environ['PATH'] +=  r'D:\ProgrammingProject\chromedriver.exe'
-driver = webdriver.Chrome()
-driver.get('https://www.facebook.com/')
-driver.implicitly_wait(10)
-
-
-
-def login(em, pss):
-    email = driver.find_element(By.ID, 'email')
-    password = driver.find_element(By.ID, 'pass')
-    login = driver.find_element(By.CSS_SELECTOR, 'button[name="login"]')
-
-    email.send_keys(em)
-    password.send_keys(pss)
-    login.click()
+from PyQt5.QtGui import QIcon
 
 
-login("Khent", 'IamHandsome')
+
+
+class mainGui(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        # Set Windows style 
+        self.setGeometry(200,200, 400,300)
+        self.setWindowTitle("This is my App")
+        self.setStyleSheet('background-color:white')
+        self.setWindowIcon(QIcon('E:\Programming\WebAutomation Materials\ActualCodes\GUI_PyQt5\PyQt5 Course\icons\Kickstarter.png'))
+        # self.setWindowOpacity(0.8) # set Windows opacity
+
+        self.create_buttons()
+        self.vertical_layout = QVBoxLayout()
+
+    def create_buttons(self):
+        
+        self.vertical_layout.addWidget(button_1)
+        self.vertical_layout.addWidget(button_2)
+        self.vertical_layout.addWidget(button_3)
+
+        
+        button_1 = QPushButton('start', self)
+        button_1.setGeometry(100, 100, 100,50)
+        button_1.setStyleSheet('color:pink')
+        button_1.setStyleSheet('background-color:pink')
+        button_1.clicked.connect(self.clicked_button_1)
+
+        button_2 = QPushButton('start 2', self)
+        button_2.setGeometry(100, 150, 100,50)
+        button_2.setStyleSheet('color:pink')
+        button_2.setStyleSheet('background-color:pink')
+        button_2.clicked.connect(self.clicked_button_2)
+
+        button_3 = QPushButton('start 2', self)
+        button_3.setGeometry(100, 200, 100,50)
+        button_3.setStyleSheet('color:pink')
+        button_3.setStyleSheet('background-color:pink')
+        button_3.clicked.connect(self.clicked_button_3)
+    
+
+    def clicked_button_1(self):
+        print('Button 1 is clicked')
+
+    def clicked_button_2(self):
+        print('Button 2 is clicked')
+
+    def clicked_button_3(self):
+        print('Button 3 is clicked')
+        
+        
+
+
+
+
+
+
+app = QApplication(sys.argv)
+window = mainGui()
+window.show()
+
+# Main event loop
+sys.exit(app.exec_())
